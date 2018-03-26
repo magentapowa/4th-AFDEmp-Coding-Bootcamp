660d1c409b25b3051a84c95ad31862ddeb514c3f
Unknown <magentapowa@gmail.com>
2018-03-26 22:51:28 +0300

    WEx02

---
+++ CB4_CS_WEx02/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CB4_CS_WEx02
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Shape s1 = new Shape();
+            s1.SetColor(Color.White);
+            Console.WriteLine(s1.GetInfo());
+
+
+            Rectangle r1 = new Rectangle(20, 5);
+            Rectangle r2 = new Rectangle(2, 8);
+            Rectangle r3 = new Rectangle(1, 1);
+            Rectangle r4 = new Rectangle(5, 9);
+            r1.SetColor(Color.Orange);
+            r2.SetColor(Color.Magenta);
+            r3.SetColor(Color.Blue);
+            r4.SetColor(Color.White);
+
+            Square sq1 = new Square(10);
+            Square sq2 = new Square(20);
+            Square sq3 = new Square(7);
+            sq1.SetColor(Color.Black);
+            sq2.SetColor(Color.Orange);
+            sq3.SetColor(Color.Red);
+
+            List<Rectangle> myShapeList = new List<Rectangle>();
+            myShapeList.Add(r1);
+            myShapeList.Add(r2);
+            myShapeList.Add(sq1);
+            myShapeList.Add(sq2);
+            myShapeList.Add(r3);
+            myShapeList.Add(sq3);
+            myShapeList.Add(r4);
+
+            Console.WriteLine("\nMy list:");
+            for (int i = 0; i < myShapeList.Count; i++)
+            {
+                Console.WriteLine(myShapeList[i] + $"   Area= {myShapeList[i].GetArea()}");
+            }
+
+            myShapeList.Sort();
+
+            Console.WriteLine("\nMy list after sorting:");
+            for (int i = 0; i < myShapeList.Count; i++)
+            {
+                Console.WriteLine(myShapeList[i] + $"   Area= {myShapeList[i].GetArea()}");
+            }
+
+
+        }
+    }
+}
---
+++ CB4_CS_WEx02/Shape.cs
@@ -0,0 +1,146 @@
+﻿using System;
+namespace CB4_CS_WEx02
+{
+    public enum Color
+    {
+        Red,
+        Blue,
+        Magenta,
+        Orange,
+        Black,
+        White
+    }
+
+    //
+    // Shape 
+    //
+    public class Shape
+    {
+        // Fields
+        private Color _color;
+
+        //Properties
+        public Color GetColor
+        {
+            get
+            {
+                return _color;
+            }
+        }
+
+        // Methods
+        public void SetColor(Color color)
+        {
+            this._color = color;
+        }
+
+        public virtual string GetInfo()
+		{    
+		    return $"Shape of color {this._color}.";
+		}
+
+		public override string ToString()
+		{
+            return GetInfo();
+		}
+	}
+
+
+    //
+    // Rectangle
+    //
+    public class Rectangle : Shape, IComparable, IEquatable<Rectangle>
+    {
+        // Fields
+        private string _name = "Rectangle";
+        private double _height;
+        private double _width;
+
+        // Properties
+        public double Height
+        {
+            get{
+                return _height;
+            }
+        }
+        public double Width
+        {
+            get {
+                return _width;
+            }
+        }
+
+        // Methods
+        public virtual double GetArea()
+        {
+            return this._width * this._height;
+        }
+
+        public override string GetInfo()
+        {
+            return $"{this._name} of color {this.GetColor}, height {this._height} and width {this._width}.";
+        }
+
+        public override string ToString()
+        {
+            return this.GetInfo();
+        }
+
+        public int CompareTo(object obj)
+        {
+            Rectangle other = obj as Rectangle;
+            if (this.GetArea() > other.GetArea())
+            {
+                return 1;
+            }
+            else if (this.GetArea() == other.GetArea())
+            {
+                return 0;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        public bool Equals(Rectangle other)
+        {
+            return this.GetArea() == other.GetArea();
+        }
+
+        // Constructor
+        public Rectangle(double height, double width)
+        {
+            this._width = width;
+            this._height = height;
+        }
+    }
+
+
+    //
+    // Square
+    //
+    public sealed class Square : Rectangle, IComparable
+    {
+        private string _name = "Square";
+        private double _side;
+
+		public override string GetInfo()
+		{
+            return $"{this._name} of color {this.GetColor} and side {this._side}.";
+		}
+
+        public override string ToString()
+        {
+            return this.GetInfo();
+        }
+
+		// Constructor
+        public Square(double side) : base(side, side)
+        {
+            this._side = side;
+
+        }
+    }
+
+}
